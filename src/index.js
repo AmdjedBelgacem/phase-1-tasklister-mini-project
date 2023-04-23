a0c6b1c175fb57d64e9338f9bf8efa216b3f423f
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-04-24 01:59:39 +0300

    Missing Stretch Deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+//Stretch Deliverables will be added accordingly
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const task = document.querySelector('#new-task-description');
+  const button = document.querySelector('#submit-btn');
+  const todo = document.querySelector("#tasks");
+
+  button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const adding = task.value;
+    const toadd = document.createElement("li");
+    toadd.textContent = adding;
+    todo.appendChild(toadd);
+    task.value="";
+  })
 });
